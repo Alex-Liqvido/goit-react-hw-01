11880373feffbcf7c63555362df7cabb91b9701a
AleksKraken <xxxaleks85xxx@gmail.com>
2025-01-16 15:47:37 +0200

    t4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
 import React from "react";
 import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import userData from "./userData.json";
+import friends from "./friends.json";
+import transactions from "./transactions.json";
 import './App.css'
-
 const App = () => {
   return (
     <div id="root">
@@ -14,9 +16,12 @@ const App = () => {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
      
     </div>
   );
 };
+
 
 export default App
